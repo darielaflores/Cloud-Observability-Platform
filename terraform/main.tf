c0629ecb82e3c56e2f9aa99e2c8cccda7abe284d
Dariela <deilyn.d.flores@gmail.com>
2026-05-27 21:58:55 +0200

    Cloud Observability Platform

--- terraform/main.tf
+++ terraform/main.tf
@@ -314,7 +314,7 @@ resource "azurerm_dashboard_grafana" "main" {
   name                              = "grafana-monitoring-lab"
   location                          = azurerm_resource_group.main.location
   resource_group_name               = azurerm_resource_group.main.name
-  grafana_major_version             = "11"
+  grafana_major_version             = "10"
   api_key_enabled                   = true
   deterministic_outbound_ip_enabled = false
   public_network_access_enabled     = true
